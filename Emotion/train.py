a53c91290e501f6525377ed16b57d92a10fe3c67
RailGiz <91553116+RailGiz@users.noreply.github.com>
2024-04-20 21:00:05 +0300

    Create train.py

---
+++ Emotion/train.py
@@ -0,0 +1,154 @@
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import cv2
+from tensorflow.keras.applications import VGG16, InceptionResNetV2
+from tensorflow.keras import regularizers
+from tensorflow.keras.optimizers import Adam, RMSprop, SGD, Adamax
+from sklearn.metrics import confusion_matrix
+import itertools
+
+train_dir = "emotiondata/train"
+test_dir = "emotiondata/test"
+
+img_size = 48
+
+train_datagen = ImageDataGenerator(
+    width_shift_range = 0.1,
+    height_shift_range = 0.1,
+    horizontal_flip = True,
+    rescale = 1./255,
+    validation_split = 0.2
+)
+
+validation_datagen = ImageDataGenerator(rescale = 1./255, validation_split = 0.2)
+
+train_generator = train_datagen.flow_from_directory(
+    directory = train_dir,
+    target_size = (img_size, img_size),
+    batch_size = 64,
+    color_mode = "grayscale",
+    class_mode = "categorical",
+    subset = "training"
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    directory = test_dir,
+    target_size = (img_size, img_size),
+    batch_size = 64,
+    color_mode = "grayscale",
+    class_mode = "categorical",
+    subset = "validation"
+)
+
+epochs = 60
+batch_size = 64
+
+model = tf.keras.models.Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu', input_shape=(48, 48,1)))
+model.add(Conv2D(64,(3,3), padding='same', activation='relu' ))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128,(5,5), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(512,(3,3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(512,(3,3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(256,activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+model.add(Dense(512,activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+
+model.add(Dense(8, activation='softmax'))
+
+model.compile(
+    optimizer = Adam(lr=0.0001),
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    x = train_generator,
+    epochs = epochs,
+    validation_data = validation_generator,
+    verbose = 1
+)
+
+# Вывод графиков обучения
+fig , ax = plt.subplots(1,2)
+fig.set_size_inches(12,4)
+
+ax[0].plot(history.history['accuracy'])
+ax[0].plot(history.history['val_accuracy'])
+ax[0].set_title('Training Accuracy vs Validation Accuracy')
+ax[0].set_ylabel('Accuracy')
+ax[0].set_xlabel('Epoch')
+ax[0].legend(['Train', 'Validation'], loc='upper left')
+
+ax[1].plot(history.history['loss'])
+ax[1].plot(history.history['val_loss'])
+ax[1].set_title('Training Loss vs Validation Loss')
+ax[1].set_ylabel('Loss')
+ax[1].set_xlabel('Epoch')
+ax[1].legend(['Train', 'Validation'], loc='upper left')
+
+plt.show()
+
+# Сохранение модели
+model.save('my_model.h5')
+
+# Построение матрицы ошибок
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Предсказания модели
+Y_pred = model.predict(validation_generator)
+y_pred = np.argmax(Y_pred, axis=1)
+
+# Вычисление матрицы ошибок
+cm = confusion_matrix(validation_generator.classes, y_pred)
+
+# Вывод матрицы ошибок
+plot_confusion_matrix(cm, classes = range(8))
